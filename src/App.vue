67641f55ec6fefd0ed4fc5403f833470fff9c1cc
darksparkz1233 <pasha.komarov.2016@bk.ru>
2023-07-07 00:12:01 +0300

    add: svg animation

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,12 @@ const mq = useMq()
 </script>
 
 <template>
-  <div 
-    class="form-container"
-    :class="mq.phone ? 'form-container-phone' : 'form-container'"
-  >
-    <leftFormBanner v-if="mq.tabletPlus"/>
+  <div class="form-container" :class="mq.phone ? 'form-container-phone' : 'form-container'">
+
+
+    <leftFormBanner v-if="mq.tabletPlus" />
+
+
 
     <formView />
   </div>
@@ -22,36 +23,29 @@ const mq = useMq()
 * {
   margin: 0;
   padding: 0;
+
 }
 
-/* Background SVG Image */
-#bg-image {
-    z-index: -1;
-    position: absolute;
-
-    left: 15%;
-    top: 18%;
-}
 
 .form-container {
-  margin: 0 auto;
+  display: flex;
   height: auto;
   width: auto;
-  display: flex;
 
   background: url(./assets/bg-svg.png) no-repeat;
   background-position-x: right;
   background-position-y: bottom;
   background-size: 19.25rem;
 
+  margin: 0 auto;
   height: 30.0625rem;
-  width: 41.5625rem; 
+  width: 41.5625rem;
+
 }
 
 .form-container-phone {
   display: flex;
   background: none;
-
   height: fit-content;
   width: fit-content;
 }
--- src/components/formView.vue
+++ src/components/formView.vue
@@ -1,11 +1,7 @@
-<script setup>
-import { useMq } from 'vue3-mq'
-
-const mq = useMq()
-</script>
-
+<!-- Component to display form content depending on the route (Sign Up form & Registered User) -->
 <template>
-    <div class="form-content-container" :class="mq.phone ? 'form-content-container-phone' : 'form-content-container'">
+    <div  
+>
 
         <router-view>
 
@@ -14,39 +10,3 @@ const mq = useMq()
     </div>
 </template>
 
-<style scoped>
-* {
-    font-family: 'Work Sans';
-}
-.form-content-container-phone {
-    padding-top: 0.41rem;
-    padding-right: 0.60rem;
-    padding-left: 0.80rem;
-}
-
-.form-content {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-}
-
-.form-title {
-    font-size: 1.375rem;
-    font-weight: 500;
-}
-
-.form-subtitle {
-    font-size: 0.875rem;
-    font-family: 'Work Sans', sans-serif;
-    font-weight: 300;
-}
-
-/* Responsive area */
-.form-title-phone {
-    font-size: 1.35rem;
-}
-
-.form-subtitle-phone {
-    font-size: 0.75rem;
-}
-</style>
